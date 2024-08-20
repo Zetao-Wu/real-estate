d180974184945e67f823359aa918111082b3f484
Zetao <zetao_wu@brown.edu>
2024-08-20 17:09:21 -0400

    connected to db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+    .connect(process.env.dbURI)
+    .then(() => {
+        console.log("Connected to MongoDB")
+        app.listen(3000, () => {
+            console.log("Sever is running on port 3000");
+        });
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 
 const app = express();
-
-app.listen(3000, () => {
-    console.log('Sever is running on port 3000');
-});
