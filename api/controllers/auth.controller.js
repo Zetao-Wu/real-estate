50d8e2b90f8d04c855ffe01836da096077e2f8fa
Zetao <zetao_wu@brown.edu>
2024-08-20 23:43:17 -0400

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   try {
@@ -14,4 +15,26 @@ export const signup = async (req, res, next) => {
     next(err);
   }
 };
- 
+
+export const signin = async (req, res, next) => {
+  const { username, password } = req.body;
+  try {
+    const validUser = await User.findOne({ username });
+    if (!validUser) {
+      return next(errorHandler(404, "User Not Found"));
+    }
+    const validPassword = bcrypt.compare(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Incorrect Credentials"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    // because we do not want to return the password as part of the return output, we will deconstruct the validUser such that the password key-value will be left out and we will return the rest of the data
+    const { password: pass, ...rest} = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (err) {
+    next(err);
+  }
+};
