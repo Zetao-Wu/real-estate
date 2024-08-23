87cb005a939ca0e78fc92fde7e2c9fdaba00902d
Zetao <zetao_wu@brown.edu>
2024-08-23 16:19:41 -0400

    add sign out functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -11,6 +11,8 @@ import {
   deleteUserFailure,
   deleteUserStart,
   deleteUserSuccess,
+  signOutUserFailure,
+  signOutUserStart,
   updateUserError,
   updateUserStart,
   updateUserSuccess,
@@ -105,6 +107,21 @@ const Profile = () => {
     }
   };
 
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart())
+      const res = await fetch('api/auth/signout');
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signOutUserFailure(data.message))
+        return;
+      }
+      dispatch(deleteUserSuccess(data))
+    } catch (err) {
+      dispatch(deleteUserFailure(data.message))
+    }
+  }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -170,7 +187,7 @@ const Profile = () => {
         >
           Delete Account
         </span>
-        <span className="text-red-700 cursor-pointer hover:underline">
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer hover:underline">
           Sign Out
         </span>
       </div>
