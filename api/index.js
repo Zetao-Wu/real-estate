11d3e5ef1dafc38663dc43b11a6a4b994b8ecc69
Zetao <zetao_wu@brown.edu>
2024-08-23 16:39:04 -0400

    add create listing api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from './routes/listing.route.js';
 import cors from "cors";
 import cookieParser from "cookie-parser";
 dotenv.config();
@@ -27,6 +28,7 @@ mongoose
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
 
 // middleware for catching an error 
 app.use((err, req, res, next) => {
