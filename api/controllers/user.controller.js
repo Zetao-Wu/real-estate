86dcab9d56dcef71ed18fd3e91377f3ce5dabb54
Zetao <zetao_wu@brown.edu>
2024-08-25 01:18:47 -0400

    create get user listings api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { errorHandler } from "../utils/error.js";
-import User from '../models/user.model.js'
+import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
+import { Listing } from "../models/listing.model.js";
 
 export const test = (req, res) => {
   res.json({
@@ -10,7 +11,7 @@ export const test = (req, res) => {
 
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
-    return next(errorHandler(401, 'You can only update your own account!'));
+    return next(errorHandler(401, "You can only update your own account!"));
   try {
     if (req.body.password) {
       req.body.password = await bcrypt.hash(req.body.password, 10);
@@ -39,12 +40,25 @@ export const updateUser = async (req, res, next) => {
 
 export const deleteUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
-    return next(errorHandler(401, 'You can only delete your own account!'));
+    return next(errorHandler(401, "You can only delete your own account!"));
   try {
     await User.findByIdAndDelete(req.params.id);
-    res.clearCookie('access_token');
-    res.status(200).json('User has been deleted!');
+    res.clearCookie("access_token");
+    res.status(200).json("User has been deleted!");
   } catch (error) {
     next(error);
+  }
+};
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id})
+      res.status(200).json(listings)
+    } catch (error) {
+      next(err);
+    }
+  } else {
+    return next(errorHandler(401, "You can only view your own listings!"));
   }
 };
